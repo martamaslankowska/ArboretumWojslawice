cb70727d61083b4cf52a1639940610745ec0b67b
Marta <maslankowska.marta@gmail.com>
2018-04-08 14:12:52 +0200

    PriceActivity (and layout) prepared for testing database connection.

--- app/src/main/java/arboretum/arboretumwojslawice/View/activities/PriceActivity.java
+++ app/src/main/java/arboretum/arboretumwojslawice/View/activities/PriceActivity.java
@@ -4,10 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import arboretum.arboretumwojslawice.R;
 
 public class PriceActivity extends AppCompatActivity {
+
+    TextView type1;
+    TextView price1;
+    TextView description1;
+    TextView type2;
+    TextView price2;
+    TextView description2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +30,18 @@ public class PriceActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        type1 = findViewById(R.id.type1);
+        price1 = findViewById(R.id.price1);
+        description1 = findViewById(R.id.description1);
+        type2 = findViewById(R.id.type2);
+        price2 = findViewById(R.id.price2);
+        description2 = findViewById(R.id.description2);
+
+        price1.setText("5,- zł");
+
     }
+
 
     public boolean onOptionsItemSelected(MenuItem item) {
         // handle arrow click here
