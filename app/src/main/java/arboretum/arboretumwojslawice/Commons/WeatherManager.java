2a41d4b427be983a817ecddc454d2780cf00915d
Marta <maslankowska.marta@gmail.com>
2018-05-10 02:25:56 +0200

    Weather forecast API added.

---
+++ app/src/main/java/arboretum/arboretumwojslawice/Commons/WeatherManager.java
@@ -0,0 +1,114 @@
+package arboretum.arboretumwojslawice.Commons;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class WeatherManager {
+
+    private static final String OPEN_WEATHER_MAP_URL =
+            "http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric";
+
+    private static final String OPEN_WEATHER_MAP_API = "3c1062023de1612eea5fee84ef1fb5ac\n";
+    // Insert your Weather API Here
+
+    public interface AsyncResponse {
+        void processFinish(String output1, String output2, String output3, String output4, String output5, String output6);
+    }
+
+
+    public static class placeIdTask extends AsyncTask<String, Void, JSONObject> {
+
+        public AsyncResponse delegate = null;
+        //Call back interface
+
+        public placeIdTask(AsyncResponse asyncResponse) {
+            delegate = asyncResponse;
+            //Assigning call back interface through constructor
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+
+            JSONObject jsonWeather = null;
+            try {
+                jsonWeather = getWeatherJSON(params[0], params[1]);
+            } catch (Exception e) {
+                Log.d("Error", "Cannot process JSON results", e);
+            }
+
+
+            return jsonWeather;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if(json != null){
+                    JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+                    JSONObject main = json.getJSONObject("main");
+                    DateFormat df = DateFormat.getDateTimeInstance();
+
+
+                    String city = json.getString("name").toUpperCase(Locale.US) + ", " + json.getJSONObject("sys").getString("country");
+                    String description = details.getString("description").toUpperCase(Locale.US);
+                    String temperature = String.format("%.2f", main.getDouble("temp"))+ "°";
+                    String humidity = main.getString("humidity") + "%";
+                    String pressure = main.getString("pressure") + " hPa";
+                    String updatedOn = df.format(new Date(json.getLong("dt")*1000));
+
+                    delegate.processFinish(city, description, temperature, humidity, pressure, updatedOn);
+
+                }
+            } catch (JSONException e) {
+
+            }
+
+
+
+        }
+    }
+
+    public static JSONObject getWeatherJSON(String lat, String lon){
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_MAP_URL, lat, lon));
+            HttpURLConnection connection =
+                    (HttpURLConnection)url.openConnection();
+
+            connection.addRequestProperty("x-api-key", OPEN_WEATHER_MAP_API);
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            StringBuffer json = new StringBuffer(1024);
+            String tmp="";
+            while((tmp=reader.readLine())!=null)
+                json.append(tmp).append("\n");
+            reader.close();
+
+            JSONObject data = new JSONObject(json.toString());
+
+            // This value will be 404 if the request was not successful
+            if(data.getInt("cod") != 200){
+                return null;
+            }
+
+            return data;
+        }catch(Exception e){
+            return null;
+        }
+    }
+
+
+
+}
--- app/src/main/java/arboretum/arboretumwojslawice/View/fragments/HomeFragment.java
+++ app/src/main/java/arboretum/arboretumwojslawice/View/fragments/HomeFragment.java
@@ -1,15 +1,18 @@
 package arboretum.arboretumwojslawice.View.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import javax.inject.Inject;
 
 import arboretum.arboretumwojslawice.Commons.DividerItemDecoration;
+import arboretum.arboretumwojslawice.Commons.WeatherManager;
 import arboretum.arboretumwojslawice.R;
 import arboretum.arboretumwojslawice.View.adapter.NewsAdapter;
 import arboretum.arboretumwojslawice.ViewModel.NewsViewModel;
@@ -17,21 +20,7 @@ import dagger.android.support.DaggerFragment;
 
 public class HomeFragment extends DaggerFragment {
 
-//    protected RecyclerView mRecyclerView;
-//    private static final String KEY_LAYOUT_MANAGER = "fragment_route";
-//    protected LayoutManagerType mCurrentLayoutManagerType;
-
-//    @Inject
-//    protected NewsViewModel newsViewModel;
-//    protected RecyclerView.LayoutManager mLayoutManager;
-
-//    @Inject
-//    protected NewsAdapter mAdapter;
-
-//    private enum LayoutManagerType {
-//        GRID_LAYOUT_MANAGER,
-//        LINEAR_LAYOUT_MANAGER
-//    }
+    TextView weather;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,47 +28,27 @@ public class HomeFragment extends DaggerFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
-//        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-//
-//        mRecyclerView = rootView.findViewById(R.id.news_recycler_view);
-//        mRecyclerView.addItemDecoration(new DividerItemDecoration(this.getContext(), 0));
-//        mRecyclerView.setAdapter(mAdapter);
-//        mAdapter.setData(newsViewModel.getData());
-//
-//        mLayoutManager = new LinearLayoutManager(getActivity());
-//
-//        if (savedInstanceState != null) {
-//            // Restore saved layout manager type.
-//            mCurrentLayoutManagerType = (HomeFragment.LayoutManagerType) savedInstanceState
-//                    .getSerializable(KEY_LAYOUT_MANAGER);
-//        }
-//
-//        setRecyclerViewLayoutManager();
-//
-//        return rootView;
+        return view;
     }
 
-//    public void setRecyclerViewLayoutManager() {
-//        int scrollPosition = 0;
-//
-//        // If a layout manager has already been set, get current scroll position.
-//        if (mRecyclerView.getLayoutManager() != null) {
-//            scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
-//                    .findFirstCompletelyVisibleItemPosition();
-//        }
-//
-//        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-//        mRecyclerView.scrollToPosition(scrollPosition);
-//    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-//    @Override
-//    public void onSaveInstanceState(Bundle savedInstanceState) {
-//        // Save currently selected layout manager.
-//        savedInstanceState.putSerializable(KEY_LAYOUT_MANAGER, mCurrentLayoutManagerType);
-//        super.onSaveInstanceState(savedInstanceState);
-//    }
+        weather = view.findViewById(R.id.weatherTextView);
+
+        WeatherManager.placeIdTask asyncTask = new WeatherManager.placeIdTask(new WeatherManager.AsyncResponse() {
+            public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn) {
+                String weatherText = weather_city + " - " + weather_description + ", " + weather_temperature;
+                weather.setText(weatherText);
+            }
+        });
+        asyncTask.execute("50.7163", "16.835"); //  asyncTask.execute("Latitude", "Longitude")
+
+
+
+    }
 }
