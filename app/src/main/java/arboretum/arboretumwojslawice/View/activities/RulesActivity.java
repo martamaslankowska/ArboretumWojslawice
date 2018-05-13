eb38e6b48dbc8579dc4ae68975a9204caa3ea093
Barbara <b.wedrychowicz@wp.pl>
2018-05-13 23:46:04 +0200

    rules activity

--- app/src/main/java/arboretum/arboretumwojslawice/View/activities/RulesActivity.java
+++ app/src/main/java/arboretum/arboretumwojslawice/View/activities/RulesActivity.java
@@ -4,10 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import arboretum.arboretumwojslawice.R;
 
 public class RulesActivity extends AppCompatActivity {
+
+    TextView title;
+    TextView subtitle;
+    TextView content1;
+    TextView content2;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,18 @@ public class RulesActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
         /* /toolbar */
+
+        //title = findViewById(R.id.rules_title);
+        subtitle = findViewById(R.id.rules_subtitle);
+        content1 = findViewById(R.id.rules_content1);
+        content2 = findViewById(R.id.rules_content2);
+        img = findViewById(R.id.rules_image);
+
+        //title.setText(R.string.rules_title);
+        subtitle.setText(R.string.rules_subtitle);
+        content1.setText(R.string.rules_content_part1);
+        //img.setBackgroundResource(R.drawable.rules_pictures);
+        content2.setText(R.string.rules_content_part2);
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
