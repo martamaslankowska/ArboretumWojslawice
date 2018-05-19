933bcd78bc1eb3ef525f02b3d642d59f30d54342
Michal <228007@student.pwr.edu.pl>
2018-05-20 01:05:13 +0200

    EventDetail layouts are better.

--- app/src/main/java/arboretum/arboretumwojslawice/View/viewholder/EventDetailViewHolder.java
+++ app/src/main/java/arboretum/arboretumwojslawice/View/viewholder/EventDetailViewHolder.java
@@ -16,19 +16,24 @@ public class EventDetailViewHolder extends BindingViewHolder<Event, EventDetailR
         binding.setEvent(item);
         binding.executePendingBindings();
 
-//        if(item.getDescription() == null)
-//        {
-//            binding.eventDetailDescription.setVisibility(View.GONE);
-//        }
-//
-//        if(item.getTimeEnd() == 0)
-//        {
-//            binding.eventDetailEndHour.setVisibility(View.GONE);
-//        }
-//
-//        if(item.getTimeBegin() == 0)
-//        {
-//            binding.eventDetailStartHour.setVisibility(View.GONE);
-//        }
+        if(item.getDescription() == null)
+        {
+            binding.eventDetailDescription.setVisibility(View.GONE);
+        }
+
+        if(item.getName() == null)
+        {
+            binding.eventDetailName.setVisibility(View.GONE);
+        }
+
+        if(item.getTimeEnd() == 0)
+        {
+            binding.eventDetailEndHour.setVisibility(View.GONE);
+        }
+
+        if(item.getTimeBegin() == 0)
+        {
+            binding.eventDetailStartHour.setVisibility(View.GONE);
+        }
     }
 }
