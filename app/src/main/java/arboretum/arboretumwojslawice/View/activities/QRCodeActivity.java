466e1c2cb0dfaf25ba793470cf1686079c2dcd32
WeronikaPiotrowska <nimbusek123@gmail.com>
2018-05-20 17:52:46 +0200

    Simple QrCode reading numbers

--- app/src/main/java/arboretum/arboretumwojslawice/View/activities/QRCodeActivity.java
+++ app/src/main/java/arboretum/arboretumwojslawice/View/activities/QRCodeActivity.java
@@ -1,8 +1,27 @@
 package arboretum.arboretumwojslawice.View.activities;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.SparseArray;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 import javax.inject.Inject;
 
@@ -14,6 +33,15 @@ public class QRCodeActivity extends DaggerAppCompatActivity {
 
     @Inject
     QRCodeViewModel qrCodeViewModel;
+
+    SurfaceView cameraPreview;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    final int requestCameraPermissionID = 1001;
+    boolean firstLoop = true;
+
+    public static final String BUNDLE = "BUNDLE";
+    public static final String PLANT_ID = "PLANT_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +58,88 @@ public class QRCodeActivity extends DaggerAppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
         /* /toolbar */
+
+        cameraPreview = findViewById(R.id.cameraSurfaceView);
+
+        barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
+        cameraSource = new CameraSource.Builder(this, barcodeDetector).build();
+
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    //Request permission
+                    ActivityCompat.requestPermissions(QRCodeActivity.this, new String[]{Manifest.permission.CAMERA}, requestCameraPermissionID);
+                    return;
+                }
+                try {
+                    cameraSource.start(cameraPreview.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> QRcodes = detections.getDetectedItems();
+                if (QRcodes.size() != 0 && firstLoop) {
+                    firstLoop = false;
+                    Intent intent = new Intent(QRCodeActivity.this, PlantDetailActivity.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putInt(PLANT_ID, Integer.valueOf(QRcodes.valueAt(0).displayValue));
+                    intent.putExtra(BUNDLE, bundle);
+                    startActivity(intent);
+                    finish();
+
+//                    intent.putExtra("plantName", QRcodes.valueAt(0).displayValue);
+//                    startActivity(intent);
+//                    finish();
+
+                    Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                    vibrator.vibrate(200);
+
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case requestCameraPermissionID: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraPreview.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            break;
+        }
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
