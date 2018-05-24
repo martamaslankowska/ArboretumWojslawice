ecce02510b86c802f030df1c8bdce60ce6d9be50
Marta <maslankowska.marta@gmail.com>
2018-05-24 02:04:11 +0200

    Opening hours activity tuned - Glide added and gradle changed.

--- app/src/main/java/arboretum/arboretumwojslawice/View/activities/OpeningHoursActivity.java
+++ app/src/main/java/arboretum/arboretumwojslawice/View/activities/OpeningHoursActivity.java
@@ -1,18 +1,29 @@
 package arboretum.arboretumwojslawice.View.activities;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 import arboretum.arboretumwojslawice.R;
 
 public class OpeningHoursActivity extends AppCompatActivity {
 
+    ImageView gifImageView;
+    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_hours);
+        context = getApplicationContext();
 
         /* toolbar */
         Toolbar toolbar = findViewById(R.id.toolbar_back);
@@ -24,6 +35,24 @@ public class OpeningHoursActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
         /* /toolbar */
+
+
+        gifImageView = findViewById(R.id.gifImageView);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                // load and run gif for one time only
+                GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(gifImageView, 1);
+                Glide.with(context)
+                        .load(R.drawable.bird_test)
+                        .into(imageViewTarget);
+
+            }
+        }, 1500); // 1.5 second delay
+
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
